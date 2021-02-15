51bf29e7f1bc62376430b5580104e15879ad7736
Ron Dorn <rdorn@salesforce.com>
2021-02-15 09:02:54 -0800

    Initial Commit

---
+++ CiPol/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CiPol
+//
+//  Created by Ron Dorn on 2/15/21.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CiPol/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CiPol
+//
+//  Created by Ron Dorn on 2/15/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
