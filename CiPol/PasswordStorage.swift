607feab215e61173e8fb7fd32fe3f8f9ce6baf83
Ron Dorn <rdorn@salesforce.com>
2021-02-25 13:02:21 -0800

    Add KeyChainSwift directly into the tool instead of using CocaPods

--- CiPol/PasswordStorage.swift
+++ CiPol/PasswordStorage.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import KeychainSwift
+
 
 class PasswordStorage {
     
--- Pods/Target Support Files/Pods-CiPol/Pods-CiPol-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CiPol : NSObject
-@end
-@implementation PodsDummy_Pods_CiPol
-@end
