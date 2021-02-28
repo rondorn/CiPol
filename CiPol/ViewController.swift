c73ec179d136d862d368d1e3b13052f1fa1c0dbb
Ron Dorn <rdorn@salesforce.com>
2021-02-28 12:14:39 -0800

    Fixed series of small UI issues

--- CiPol/ViewController.swift
+++ CiPol/ViewController.swift
@@ -57,7 +57,10 @@ class ViewController: NSViewController, NSWindowDelegate  {
         self.view.window?.styleMask.remove(.fullScreen)
         self.view.window?.styleMask.remove(.miniaturizable)
         self.view.window?.styleMask.remove(.resizable)
+        
+        preferredContentSize = view.frame.size
     }
+    
     
     @objc func reopenWindow(){
         if (view.window?.screen ?? NSScreen.main) != nil {
